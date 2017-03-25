35e00e450919aa0a6e2d0840614214b807abae38
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-03-24 22:17:40 -0700

    Add sample go-wrk instruction into a cheetsheet docset

---
+++ go-lang.rb
@@ -0,0 +1,17 @@
+cheatsheet do
+  title 'Go lang: useful tools'
+  docset_file_name 'go-lang'
+  keyword 'gotools'
+  source_url 'https://github.com/antontsv/go-lang-cheet-sheet'
+
+  category do
+    id 'Go lang benchmarking'
+
+    entry do
+      command 'go-wrk'
+      name 'HTTP benchmaking tool'
+      notes 'Install tool using: `go get github.com/tsliwowicz/go-wrk`'
+    end
+  end
+
+end
